925f233a293e538d8fb7561e758cc80410448ff9
Picur <pawel.picur@hpe.com>
2017-08-18 13:34:30 +0200

    language in home

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { firebaseApp } from '../firebase';
 import { connect } from 'react-redux';
 import Anchor from 'grommet/components/Anchor';
+import Form from 'grommet/components/Form';
+import Select from 'grommet/components/Select';
+import FormField from 'grommet/components/FormField';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 class Index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      language: {
+        label: read_cookie('LanguageReminders').label,
+        value: read_cookie('LanguageReminders').value
+      }
     }
   }
 
@@ -15,10 +23,25 @@ class Index extends Component {
     firebaseApp.auth().signOut();
   }
 
+  selectLanguage(event){
+    const lang = event.option
+    bake_cookie('LanguageReminders', lang)
+    this.setState({language:lang})
+  }
+
   render() {
   return (
       <div>
           <h3>Goals</h3>
+          <Form>
+            
+            <FormField label="Language">
+              <Select placeHolder='Language'
+                options={[{value:'en', label:'English'}, {value:'pl', label:'Polish'}, {value:'fr', label:'French'}, {value:'es', label:'Spanish'}]}
+                value={this.state.language.label}
+                onChange={event => this.selectLanguage(event)} />
+            </FormField>
+          </Form>
           <div>Add Goal</div>
           <div>Goal List</div>
           <Anchor label="Reminder" path={'/Reminder'}/>
--- src/js/components/Reminder.jsx
+++ src/js/components/Reminder.jsx
@@ -5,7 +5,6 @@ import moment from 'moment';
 import Anchor from 'grommet/components/Anchor';
 import DateTime from 'grommet/components/DateTime';
 import Form from 'grommet/components/Form';
-import Select from 'grommet/components/Select';
 import FormField from 'grommet/components/FormField';
 import TextInput from 'grommet/components/TextInput';
 import { bake_cookie, read_cookie } from 'sfcookies';
@@ -17,15 +16,11 @@ class Reminder extends Component {
       text: '',
       dueDate: '',
       language: {
-        label: '',
-        value: ''
+        label: read_cookie('LanguageReminders').label,
+        value: read_cookie('LanguageReminders').value
       }
     }
   }
-
-componentDidMount(){
-  this.loadLastLang();
-}
 
 addReminder() {
   console.log('this', this);
@@ -38,16 +33,6 @@ deleteReminder(id) {
   this.props.deleteReminder(id);
 }
 
-selectLanguage(event){
-  const lang = event.option
-  bake_cookie('LanguageReminders', lang)
-  this.setState({language:lang})
-}
-
-loadLastLang(){
-  const lastlang = read_cookie('LanguageReminders');
-  this.setState({language:lastlang})
-}
 
 renderReminders() {
     console.log('this.props przed map', this.props)
@@ -87,13 +72,6 @@ render() {
       <div className="form-inline reminder-form">
         <div className="form-group">
           <Form>
-            
-            <FormField label="Language">
-              <Select placeHolder='Language'
-                options={[{value:'en', label:'English'}, {value:'pl', label:'Polish'}, {value:'fr', label:'French'}, {value:'es', label:'Spanish'}]}
-                value={this.state.language.label}
-                onChange={event => this.selectLanguage(event)} />
-            </FormField>
 
             <FormField label="Reminder">
               <TextInput ref='reminder_input' placeholder="To do..." style={{border:'none'}} value={this.state.text} onChange={event => this.setState({text: event.target.value})} />
